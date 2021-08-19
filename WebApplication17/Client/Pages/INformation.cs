bc4a337bba81e8d819e748b5f3858f16331ce68c
YATENDRA AWANA <yka6739@gmail.com>
2021-08-19 09:16:16 +0530

    Add project files.

---
+++ WebApplication17/Client/Pages/INformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication17.Client.Shared
+{
+    public class INformation
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public DateTime StartingDate { get; set; }
+        public string Banner { get; set; }
+        public string TeacherName { get; set; }
+        public string Post { get; set; }
+    }
+}
